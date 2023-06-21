3268117fcc4ba0ded586b5eaac70f3f2fd0c8d6d
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-20 22:42:42 -0700

    Try to generate some vtubers.

--- jsonformer_personality_popstar.py
+++ jsonformer_personality_popstar.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: MIT
 
 # pip install git+https://github.com/V-Sekai/jsonformer.git
-# pip install accelerate transformers accelerate bitsandbytes optimum
+# pip install accelerate transformers bitsandbytes optimum
 # micromamba install cudatoolkit -c conda-forge
 # pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
@@ -24,36 +24,44 @@ json_schema = {
   "$schema": "http://json-schema.org/draft-07/schema#",
   "type": "object",
   "properties": {
-    "agent": {
+    "personality": {
       "type": "string",
-      "description": "The unique identifier for the AI robot assistant."
+      "description": "The unique identifier for the personality."
     },
     "knowledge": {
       "type": "string",
-      "description": "The specific area of expertise or knowledge of the AI robot assistant."
+      "description": "The specific area of expertise or knowledge of the personality."
     },
     "context": {
       "type": "object",
       "properties": {
-        "technical_skills": {
+        "skills": {
           "type": "array",
           "items": {
             "type": "string"
           },
-          "description": "A list of technical skills possessed by the AI robot assistant."
+          "description": "A list of skills possessed by the personality."
         }
       },
-      "required": ["technical_skills"]
+      "required": ["skills"]
     }
   },
-  "required": ["agent", "knowledge", "context"]
+  "required": ["personality", "knowledge", "context"]
 }
 
-prompt = (
-    """You are a character designer generate 4 different personalities for these superheros."""
-)
+vtuber_names = [
+    "Alyssa Hartfield",
+    "Cameron Oakley",
+    "Evelyn Blackwood",
+    "Jordan Fairchild",
+    "Morgan Westfall",
+    "Riley Stonebridge",
+    "Taylor Greenleaf",
+    "Alexis Ironwood"
+]
 
-jsonformer = Jsonformer(model, tokenizer, json_schema, prompt)
-generated_data = jsonformer()
-
-print(generated_data)
+for name in vtuber_names:
+    prompt = f""""Generate VTuber {name}."""
+    jsonformer = Jsonformer(model, tokenizer, json_schema, prompt)
+    generated_data = jsonformer()
+    print(generated_data)
