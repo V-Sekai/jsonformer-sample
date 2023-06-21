2879d51ffc458bfb1dc9b196f305c122d597d11c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-21 01:10:09 -0700

    Save architecture.

--- jsonformer_personality_popstar.py
+++ jsonformer_personality_popstar.py
@@ -19,136 +19,133 @@ model.config.use_cache = True
 
 model = BetterTransformer.transform(model)
 
-name_schema = {
-  "type": "string",
-  "description": "The name of the VTuber, which can be a combination of real or fictional words.",
-  "minLength": 2
-}
-
-avatar_schema = {
-  "type": "string",
-  "enum": ["2D", "3D"],
-  "description": "A 2D or 3D digital representation of the VTuber, often designed with unique features and characteristics."
-}
-
-uniqueIdentifier_schema = {
-  "type": "string",
-  "description": "The unique identifier for the personality.",
-  "minLength": 3,
-  "maxLength": 16
-}
-
-knowledge_schema = {
-  "type": "string",
-  "description": "The specific area of expertise or knowledge of the personality.",
-}
-
-skills_schema = {
-  "type": "array",
-  "items": {
-    "type": "string"
+schema = {
+  "$schema": "http://json-schema.org/draft-07/schema#",
+  "type": "object",
+  "properties": {
+    "name": {
+      "type": "string",
+      "description": "The name of the VTuber, which can be a combination of real or fictional words.",
+      "minLength": 2
+    },
+    "avatar": {
+      "type": "string",
+      "enum": ["2D", "3D"],
+      "description": "A 2D or 3D digital representation of the VTuber, often designed with unique features and characteristics."
+    },
+    "uniqueIdentifier": {
+      "type": "string",
+      "description": "The unique identifier for the personality.",
+      "minLength": 3,
+      "maxLength": 16
+    },
+    "skills": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      },
+      "description": "A list of skills possessed by the personality.",
+      "minItems": 1
+    },
+    "voice": {
+      "type": "string",
+      "enum": ["synthesized", "human"],
+      "description": "The voice of the VTuber, which can be either synthesized or provided by a human voice actor."
+    },
+    "backstory": {
+      "type": "string",
+      "enum": [
+        "origin",
+        "history",
+        "motivations",
+        "adventures",
+        "relationships"
+      ],
+      "description": "A fictional background story for the VTuber, which can include details about their origin, history, and motivations."
+    },
+    "content": {
+      "type": "string",
+      "enum": ["gaming", "music", "art", "educational", "variety"],
+      "description": "The type of content the VTuber creates, such as gaming, music, art, or educational videos."
+    },
+    "platform": {
+      "type": "string",
+      "enum": ["YouTube", "Twitch"],
+      "description": "The primary platform where the VTuber shares their content, such as YouTube, Twitch, or other streaming services."
+    },
+    "fanbase": {
+      "type": "string",
+      "description": "The community of fans who follow and support the VTuber's content."
+    },
+    "collaborations": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      },
+      "description": "Any collaborations the VTuber has done with other creators, both virtual and non-virtual."
+    },
+    "merchandise": {
+      "type": "string",
+      "enum": ["clothing", "accessories", "digital goods"],
+      "description": "Any physical or digital merchandise related to the VTuber, such as clothing, accessories, or digital goods."
+    },
+    "knowledge": {
+      "type": "string",
+      "description": "The specific area of expertise or knowledge of the personality."
+    },
   },
-  "description": "A list of skills possessed by the personality.",
-  "minItems": 1
+  "required": [
+    "name",
+    "avatar",
+    "uniqueIdentifier",
+    "knowledge",
+    "skills",
+    "voice",
+    "backstory",
+    "content",
+    "platform",
+    "fanbase",
+    "collaborations",
+    "merchandise"
+  ]
 }
 
-voice_schema = {
-  "type": "string",
-  "enum": ["synthesized", "human"],
-  "description": "The voice of the VTuber, which can be either synthesized or provided by a human voice actor."
-}
+def break_apart_schema(schema):
+    if "properties" not in schema:
+        return []
 
-backstory_schema = {
-  "type": "string",
-  "enum": [
-    "origin",
-    "history",
-    "motivations",
-    "adventures",
-    "relationships"
-  ],
-  "description": "A fictional background story for the VTuber, which can include details about their origin, history, and motivations.",
-}
+    properties = schema["properties"]
+    required = schema.get("required", [])
+    result = []
 
-content_schema = {
-  "type": "string",
-  "enum": ["gaming", "music", "art", "educational", "variety"],
-  "description": "The type of content the VTuber creates, such as gaming, music, art, or educational videos."
-}
+    for key, value in properties.items():
+        if "properties" in value or "items" in value:
+            if "properties" in value:
+                nested_result = break_apart_schema(value)
+            else:  # "items" in value
+                nested_result = break_apart_schema(value["items"])
 
-platform_schema = {
-  "type": "string",
-  "enum": ["YouTube", "Twitch"],
-  "description": "The primary platform where the VTuber shares their content, such as YouTube, Twitch, or other streaming services."
-}
+            result.extend(nested_result)
+        else:
+            property_schema = {
+                "$schema": "http://json-schema.org/draft-07/schema#",
+                "type": "object",
+                "properties": {
+                    key: value
+                },
+                "required": [key] if key in required else []
+            }
+            result.append(property_schema)
 
-fanbase_schema = {
-  "type": "string",
-  "description": "The community of fans who follow and support the VTuber's content.",
-}
-
-collaborations_schema = {
-  "type": "array",
-  "items": {
-    "type": "string"
-  },
-  "description": "Any collaborations the VTuber has done with other creators, both virtual and non-virtual."
-}
-
-merchandise_schema = {
-  "type": "string",
-  "enum": ["clothing", "accessories", "digital goods"],
-  "description": "Any physical or digital merchandise related to the VTuber, such as clothing, accessories, or digital goods."
-}
-
-subschemas = {
-    "name": name_schema,
-    "avatar": avatar_schema,
-    "uniqueIdentifier": uniqueIdentifier_schema,
-    "knowledge": knowledge_schema,
-    "skills": skills_schema,
-    "voice": voice_schema,
-    "backstory": backstory_schema,
-    "content": content_schema,
-    "platform": platform_schema,
-    "fanbase": fanbase_schema,
-    "collaborations": collaborations_schema,
-    "merchandise": merchandise_schema
-}
+    return result
 
 prompt = """Gura is a friendly, mischievous shark with a generally amiable personality. She has no sense of direction and often mispronounces words. Combined with her sense of laziness, this has led fans to affectionately label her a bonehead."""
 
-def generate_data(property_name, schema):
-    if schema["type"] == "array":
-        json_schema = {
-            "$schema": "http://json-schema.org/draft-07/schema#",
-            "type": "object",
-            "properties": {
-                property_name: {
-                    "type": "array",
-                    "items": schema["items"]
-                }
-            },
-            "required": [property_name]
-        }
-    else:
-        json_schema = {
-            "$schema": "http://json-schema.org/draft-07/schema#",
-            "type": "object",
-            "properties": {
-                property_name: schema
-            },
-            "required": [property_name]
-        }
-
-    jsonformer = Jsonformer(model, tokenizer, json_schema, prompt, max_string_token_length=2048)
-    generated_data = jsonformer()
-
-    return {property_name: generated_data[property_name]}
 
 merged_data = {}
-for property_name, schema in subschemas.items():
-    generated_data = generate_data(property_name, schema)
+for new_schema in break_apart_schema(schema):
+    jsonformer = Jsonformer(model, tokenizer, new_schema, prompt, max_string_token_length=2048)
+    generated_data = jsonformer()
     print(generated_data)
 
     for key, value in generated_data.items():
@@ -157,4 +154,10 @@ for property_name, schema in subschemas.items():
 print("Merged Data:")
 print(merged_data)
 
+import json
+merged_data_str = json.dumps(merged_data)
+
+jsonformer = Jsonformer(model, tokenizer, schema, merged_data_str, max_string_token_length=2048)
+final_data = jsonformer()
+print(final_data)
 
