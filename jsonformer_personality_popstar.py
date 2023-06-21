3ccd38ae63202fcc153374d5917603bef21f9ae6
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-21 09:44:10 -0700

    Pause.

--- jsonformer_personality_popstar.py
+++ jsonformer_personality_popstar.py
@@ -26,6 +26,7 @@ file_handler = logging.FileHandler('output.log')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
+
 logger.propagate = False
 
 from opentelemetry.sdk.resources import Resource
@@ -215,29 +216,14 @@ def process_prompts(prompts):
             with tracer.start_as_current_span("jsonformer_generate"):
                 generated_data = jsonformer()
                 logger.info(f"jsonformer_generate: {generated_data}")
-        
-    for key, value in generated_data.items():
-        merged_data[key] = value
+        for key, value in generated_data.items():
+            merged_data[key] = value
+
     logger.info(f"merged_data: {merged_data}")
 
 input_list = ["""
-## Character Concept: Mira the Enchanting Fox
+Steven Paul Jobs (February 24, 1955 – October 5, 2011) was an American business magnate, inventor, and investor. He was the co-founder, chairman, and CEO of Apple; the chairman and majority shareholder of Pixar; a member of The Walt Disney Company's board of directors following its acquisition of Pixar; and the founder, chairman, and CEO of NeXT. He was a pioneer of the personal computer revolution of the 1970s and 1980s, along with his early business partner and fellow Apple co-founder Steve Wozniak.
 
-**Background:** Mira is a charming, intelligent fox with a love for weaving enchantments and unraveling mysteries. She possesses a deep understanding of various subjects and takes pleasure in imparting her knowledge to others.
-
-**Personality Traits:**
-
-1. Astute and perceptive
-2. Delights in casting enchantments on friends and admirers
-3. Revels in deciphering enigmas and unraveling secrets
-4. Possesses an inquisitive nature and thrives on acquiring new knowledge
-5. Can be impish at times, but ultimately has a benevolent spirit
-
-**Appearance:**
-
-- Fox ears and tail
-- Lustrous, iridescent fur (e.g., shades of blue, purple, and silver)
-- Adorns a fashionable outfit that combines contemporary and classic elements
-- Wields a mystical staff that she employs to weave enchantments and perform illusions
+Jobs was born in San Francisco to a Syrian father and German-American mother. He was adopted shortly after his birth. Jobs attended Reed College in 1972 before withdrawing that same year. In 1974, he traveled through India seeking enlightenment before later studying Zen Buddhism. He and Wozniak co-founded Apple in 1976 to sell Wozniak's Apple I personal computer. Together the duo gained fame and wealth a year later with production and sale of the Apple II, one of the first highly successful mass-produced microcomputers. Jobs saw the commercial potential of the Xerox Alto in 1979, which was mouse-driven and had a graphical user interface (GUI). This led to the development of the unsuccessful Apple Lisa in 1983, followed by the breakthrough Macintosh in 1984, the first mass-produced computer with a GUI. The Macintosh introduced the desktop publishing industry in 1985 with the addition of the Apple LaserWriter, the first laser printer to feature vector graphics. 
 """]
 process_prompts(input_list)
