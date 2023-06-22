870d8669ef147ef325d4f56865917836acc7c543
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-22 16:16:09 -0700

    Fix.

--- main.py
+++ main.py
@@ -39,9 +39,7 @@ class Predictor(BasePredictor):
                 input_prompt: str = Input(description="Input prompt for the model"),
                 input_schema: str = Input(description="Input schema for the model")) -> str:
         with tracer.start_as_current_span("run_jsonformer"):
-            output = process_prompts_common(model, tokenizer, input_prompt, input_schema)
-            output_again_to_mix = process_prompts_common(model, tokenizer, output, input_schema)
-            return output_again_to_mix
+            return process_prompts_common(model, tokenizer, input_prompt, json.loads(input_schema))
 
 def gradio_interface(input_prompt, input_schema):
     predictor = Predictor()
@@ -51,8 +49,8 @@ def gradio_interface(input_prompt, input_schema):
 
 if __name__ == "__main__":
     
-    input_prompt ="""This emote represents a catgirl face with cat ears and a happy expression."""
-    input_schema = """{
+    input_prompt_str ="""This emote represents a catgirl face with cat ears and a happy expression."""
+    input_schema_str = """{
         "$schema": "http://json-schema.org/draft-07/schema#",
         "type": "object",
         "description": "A schema representing an animation with a name, a description, and a transition trigger. The animation occurs after the specified trigger.",
@@ -79,14 +77,20 @@ if __name__ == "__main__":
                 "description": "A trigger for transitioning between animations in the animation tree. The animation occurs after this trigger.",
                 "properties": {
                     "trigger_condition": {
+                        "minLength": 5,
+                        "maxLength": 50,
                         "type": "string",
                         "description": "The condition that must be met for the transition to occur."
                     },
                     "from_animation": {
+                        "minLength": 5,
+                        "maxLength": 50,
                         "type": "string",
                         "description": "The name of the animation to transition from."
                     },
                     "to_animation": {
+                        "minLength": 5,
+                        "maxLength": 50,
                         "type": "string",
                         "description": "The name of the animation to transition to."
                     }
@@ -106,5 +110,5 @@ if __name__ == "__main__":
         outputs=gr.components.JSON(label="Generated JSON"),
         title="JSONFormer with Gradio",
         description="Generate JSON data based on input prompt and schema.",
-        examples = [[input_prompt, input_schema]])
+        examples = [[input_prompt_str, input_schema_str]])
     iface.launch(share=True)
