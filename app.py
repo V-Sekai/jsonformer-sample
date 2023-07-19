9ff0c82cc24d1d6aa2f0a5fa69ae78e95352ad59
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-07-18 20:46:42 -0700

    Update model name and configuration in app.py
    - Update model_name variable to "mosaicml/mpt-30b"
    - Update config variable to use the new model name
    - Set attn_config['attn_impl'] to 'triton' for triton-based FlashAttention
    - Set init_device to 'cuda:0' for fast initialization directly on GPU

--- app.py
+++ app.py
@@ -24,12 +24,12 @@ def process_prompts_common(model: Any, tokenizer: Any, prompt: str, schema: Dict
             return jsonformer()
     
 
-config = transformers.AutoConfig.from_pretrained(name, trust_remote_code=True)
+model_name = "mosaicml/mpt-30b"
+config = transformers.AutoConfig.from_pretrained(model_name, trust_remote_code=True)
 config.attn_config['attn_impl'] = 'triton'  # change this to use triton-based FlashAttention
 config.init_device = 'cuda:0' # For fast initialization directly on GPU!
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
-model_name = "mosaicml/mpt-30b"
 model = AutoModelForCausalLM.from_pretrained(model_name, 
   config=config,
   device_map="auto", 
