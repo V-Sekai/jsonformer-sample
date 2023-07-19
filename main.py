cadb5cce5e6e945ebc1fe1be2ff14b290beab917
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-07-18 19:54:08 -0700

    feat: Add support for 4-bit model loading
    This commit adds support for loading the model in 4-bit format. The `load_in_4bit` parameter is set to `True` when initializing the `AutoModelForCausalLM` object. This allows for more efficient memory usage and faster inference with the specified model.

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def process_prompts_common(model: Any, tokenizer: Any, prompt: str, schema: Dict
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 model_name = "mosaicml/mpt-7b-8k"
-model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", load_in_4bit=True)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 from cog import BasePredictor, Input
